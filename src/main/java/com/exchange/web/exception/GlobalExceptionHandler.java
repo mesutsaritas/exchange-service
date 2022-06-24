2afb28e1f4176b8ec22fa39eefe529c655c5e192
Mesut Sarıtas <mesutsaritas24@gmail.com>
2022-06-25 02:46:23 +0300

    #reasons formatter exxceptionhandler

--- src/main/java/com/exchange/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/exchange/web/exception/GlobalExceptionHandler.java
@@ -27,50 +27,55 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
-    @ExceptionHandler(value = ServiceUnavailableException.class)
-    public @ResponseBody
-    ResponseEntity<ErrorResource> serviceUnavailableException(ServiceUnavailableException e) {
-        return new ResponseEntity<>(e.getErrorResource(), HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
-    }
+  @ExceptionHandler(value = ServiceUnavailableException.class)
+  public @ResponseBody
+  ResponseEntity<ErrorResource> serviceUnavailableException(ServiceUnavailableException e) {
+    return new ResponseEntity<>(e.getErrorResource(), HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+  }
 
 
-    @ExceptionHandler(value = UnsupportedCurrencyTypeException.class)
-    public @ResponseBody
-    ResponseEntity<ErrorResource> unsupportedCurrencyTypeException(UnsupportedCurrencyTypeException e) {
-        return new ResponseEntity<>(e.getErrorResource(), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler(value = UnsupportedCurrencyTypeException.class)
+  public @ResponseBody
+  ResponseEntity<ErrorResource> unsupportedCurrencyTypeException(UnsupportedCurrencyTypeException e) {
+    return new ResponseEntity<>(e.getErrorResource(), HttpStatus.BAD_REQUEST);
+  }
 
 
-    @ExceptionHandler(value = EmptyParametersException.class)
-    public @ResponseBody
-    ResponseEntity<ErrorResource> emptyParamatersException(EmptyParametersException e) {
-        return new ResponseEntity<>(e.getErrorResource(), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler(value = EmptyParametersException.class)
+  public @ResponseBody
+  ResponseEntity<ErrorResource> emptyParametersException(EmptyParametersException e) {
+    return new ResponseEntity<>(e.getErrorResource(), HttpStatus.BAD_REQUEST);
+  }
 
 
-    @NotNull
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, @NotNull HttpHeaders headers, @NotNull HttpStatus status, @NotNull WebRequest request) {
-        List<String> validationErrors = exception.getBindingResult().getFieldErrors().stream().map(error -> error.getField() + ": " + error.getDefaultMessage()).collect(Collectors.toList());
-        return getExceptionResponseEntity(request, validationErrors);
-    }
+  @NotNull
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, @NotNull HttpHeaders headers,
+      @NotNull HttpStatus status, @NotNull WebRequest request) {
+    List<String> validationErrors = exception.getBindingResult().getFieldErrors().stream()
+        .map(error -> error.getField() + ": " + error.getDefaultMessage()).collect(Collectors.toList());
+    return getExceptionResponseEntity(request, validationErrors);
+  }
 
 
-    @ExceptionHandler({ConstraintViolationException.class})
-    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException exception, WebRequest request) {
-        List<String> validationErrors = exception.getConstraintViolations().stream().map(violation -> violation.getPropertyPath() + ": " + violation.getMessage()).collect(Collectors.toList());
-        return getExceptionResponseEntity(request, validationErrors);
-    }
+  @ExceptionHandler({ConstraintViolationException.class})
+  public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException exception, WebRequest request) {
+    List<String> validationErrors = exception.getConstraintViolations().stream()
+        .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage()).collect(Collectors.toList());
+    return getExceptionResponseEntity(request, validationErrors);
+  }
 
 
-    private ResponseEntity<Object> getExceptionResponseEntity(WebRequest request, List<String> errors) {
-        final Map<String, Object> body = new LinkedHashMap<>();
-        final String errorsMessage = !CollectionUtils.isEmpty(errors) ? errors.stream().filter(StringUtils::isNotEmpty).collect(Collectors.joining(",")) : HttpStatus.BAD_REQUEST.getReasonPhrase();
-        final String path = request.getDescription(false);
-        body.put("Errors", errorsMessage);
-        body.put("Path", path);
-        body.put("Message", HttpStatus.BAD_REQUEST.getReasonPhrase());
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-    }
+  private ResponseEntity<Object> getExceptionResponseEntity(WebRequest request, List<String> errors) {
+    final Map<String, Object> body = new LinkedHashMap<>();
+    final String errorsMessage =
+        !CollectionUtils.isEmpty(errors) ? errors.stream().filter(StringUtils::isNotEmpty).collect(Collectors.joining(","))
+            : HttpStatus.BAD_REQUEST.getReasonPhrase();
+    final String path = request.getDescription(false);
+    body.put("Errors", errorsMessage);
+    body.put("Path", path);
+    body.put("Message", HttpStatus.BAD_REQUEST.getReasonPhrase());
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+  }
 
 }
