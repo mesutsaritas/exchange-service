6bf67e632f6c6b1e9be6023f108335fe058f0660
Mesut Sarıtas <mesutsaritas24@gmail.com>
2022-06-27 17:17:01 +0300

    #reasons reformat

--- src/main/java/com/exchange/config/AuditorAwareImpl.java
+++ src/main/java/com/exchange/config/AuditorAwareImpl.java
@@ -1,1 +1,15 @@
-package com.exchange.config;import java.util.Optional;import org.springframework.data.domain.AuditorAware;/** * @author msaritas */public class AuditorAwareImpl implements AuditorAware<String> {  @Override  public Optional<String> getCurrentAuditor() {    return Optional.of("ExchangeService");  }}
+package com.exchange.config;
+
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+
+/**
+ * @author msaritas
+ */
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    return Optional.of("ExchangeService");
+  }
+}
